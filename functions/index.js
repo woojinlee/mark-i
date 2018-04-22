6a64008eac1f3495180402071ba84d3ae26d3f82
woojinlee <starlikcrawling@gmail.com>
2018-04-22 15:43:46 +0900

    유저 전체 조회 allUser, 특정 유저 조회 user 함수 추가

--- functions/index.js
+++ functions/index.js
@@ -10,3 +10,35 @@ admin.initializeApp({
 exports.hello = functions.https.onRequest( (req, res) => {
   res.send('Hello!');
 });
+
+exports.allUsers = functions.https.onRequest( (req, res) => {
+  var db = admin.firestore();
+  db.collection('user').get().then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+    res.send('ok');
+  })
+  .catch((err) => {
+      console.log('Error getting documents', err);
+      res.send(err);
+  });
+});
+
+exports.user = functions.https.onRequest( (req, res) => {
+  if (req.path == null) {
+    res.send('Error: user id is null')
+    return;
+  }
+
+  var userId = req.path;
+  console.log('user id: ' + userId);  
+  var db = admin.firestore();
+  db.doc('user' + userId).get().then((doc) => {
+    console.log(doc.id, '=>', doc.data());
+    res.send('ok');
+  }).catch((err) => {
+      console.log('Error getting documents', err);
+      res.send(err);
+  });
+});
