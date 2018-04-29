663374d2b3805926511d70cc2f4e2eeb2301a8e0
Woojin Lee <starlikecrawling@gmail.com>
2018-04-29 23:33:53 +0900

    hello2

--- src/functions/http-tigger.js
+++ src/functions/http-tigger.js
@@ -5,6 +5,10 @@ export const hello = https.onRequest(async (req, res) => {
     res.send('Hello!');
 });
 
+export const hello2 = https.onRequest(async (req, res) => {
+    res.send('Hello2!');
+});
+
 export const allUsers = https.onRequest(async (req, res) => {
     const { docs: user } = await store
     .collection('user')
